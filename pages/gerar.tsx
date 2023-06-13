09f9cadc382817512521393f4874b6cda1ab982a
Gabriel Araujo <gbmsaraujo@gmail.com>
2023-06-13 20:30:36 -0300

    adding netlify

--- pages/gerar.tsx
+++ pages/gerar.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from "../src/Components/Navbar/Navbar"
 import styles from '../styles/Home.module.scss'
+import axios from "axios"
 
 export default function Gerar() {
+  const [cpfGenerated, setCpfGenerated] = useState<string>("")
+
+  const handleGenerateCpf = async ()=> {
+    const {data} = await axios.get("http://127.0.0.1:8000/gerar-cpf")
+    setCpfGenerated(data)
+  }
   return (
     <>
       <Navbar />
 
       <section className={styles.gerar}>
         <h2>Gerador de CPFs</h2>
-        <button> Clique aqui para gerar um CPF válido</button>
+        <button onClick={handleGenerateCpf}> Clique aqui para gerar um CPF válido</button>
 
-        <div>
-          <p>O CPF gerado foi: <strong>11122233345</strong></p>
-        </div>
+        {cpfGenerated && <div>
+          <p>O CPF gerado foi: <strong>{cpfGenerated}</strong></p>
+        </div>}
       </section>
       <footer className={styles.footer}>
         <p>Feito por <strong>Gabriel Araujo.</strong></p>
--- pages/validar.tsx
+++ pages/validar.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../src/Components/Navbar/Navbar';
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/Home.module.scss';
+import axios from 'axios';
 
 export default function Validar() {
+	const [cpf, setCpf] = useState('');
+	const [responseCpf, setResponseCpf] = useState<string>("");
+	const [classValid, setClassValid] = useState<boolean>(false);
+
+	const handleValidation = async () => {
+		const { data } = await axios.post('http://127.0.0.1:8000/validar-cpf', {
+			cpf: cpf,
+		});
+
+		setResponseCpf(data.message);
+		setClassValid(Boolean(data.status))
+		console.log(typeof classValid)
+	};
+
 	return (
 		<>
 			<Navbar />
@@ -11,13 +26,16 @@ export default function Validar() {
 				<h2>Validador de CPF</h2>
 				<div>
 					<label htmlFor=''>Cole aqui seu CPF:</label>
-					<input type='text' />
-					<button>Validar</button>
+					<input
+						type='text'
+						onChange={(e) => setCpf(e.target.value)}
+					/>
+					<button onClick={handleValidation}>Validar</button>
 				</div>
 
-				<div className={styles.valido}>
-					<p>O CPF xxx é Válido</p>
-				</div>
+					<div className={styles.resposta}>
+						<p> {responseCpf} </p>
+					</div>
 
 			</section>
 			<footer className={styles.footer}>
